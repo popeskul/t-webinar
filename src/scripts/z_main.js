11a40988e96734941d20cb56ad883ec3471573f7
Popeskul Pavel <peterjj91@gmail.com>
2019-04-09 15:55:59 +0300

    Fix tab navigation

--- src/scripts/z_main.js
+++ src/scripts/z_main.js
@@ -13,6 +13,26 @@ $(document).ready(function() {
     });
 });
 
+// start fix navigation
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myTabNavigation");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+// end fix navigation
+
 $('.mobile-menu').click(function(){
     $(this).toggleClass("active");
 });
